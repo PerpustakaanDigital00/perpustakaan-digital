d6fc98d4d0c48ecd9830e807df036ca8cc32a3ee
FarhanRiziq01 <farhanriziq01@gmail.com>
2023-10-08 17:04:51 +0700

    prop

--- js/proposal.js
+++ js/proposal.js
@@ -11,3 +11,15 @@ setInner("jdltujuan", data.isi.jdltujuan);
 setInner("tujuan", data.isi.tujuan);
 setInner("jdllingkup", data.isi.jdllingkup);
 setInner("isilingkup", data.isi.isilingkup);
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Ambil elemen burger dan menu
+    var burger = document.querySelector('.navbar-burger');
+    var menu = document.querySelector('#navbarMenu');
+
+    // Tambahkan event listener untuk toggle menu
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+});
